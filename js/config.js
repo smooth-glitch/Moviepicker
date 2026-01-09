c151367c4e3c2034f8e0447cea3b6ff5bb4e707a
Arjun <arjunsridhar445@gmail.com>
2026-01-09 18:59:55 +0530

    added giphs and reply feature 3

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-// TMDB + Firebase config
+// config.js
 
 window.APP_CONFIG = {
   firebaseConfig: {
@@ -8,11 +8,17 @@ window.APP_CONFIG = {
     storageBucket: "movienight-picker.firebasestorage.app",
     messagingSenderId: "169239854984",
     appId: "1:169239854984:web:a25b26739ed82a37f4c43d",
-    measurementId: "G-3P11RRJQ4K"
+    measurementId: "G-3P11RRJQ4K",
   },
+
+  // Keep it here if you want, but we also export it below.
   GIPHY_API_KEY: "8yONfoSWS51lRx1f0vpOVutdcwcLRkug",
 };
 
+// This is what your app reads everywhere else (tmdb.js, firebase-init.js, gif.js, etc.)
 window.APPCONFIG = {
   firebaseConfig: window.APP_CONFIG.firebaseConfig,
+
+  // NEW: expose it here so searchGifs() can use it
+  GIPHY_API_KEY: window.APP_CONFIG.GIPHY_API_KEY,
 };
